6fea0de13a4a33566d7b91d089c3a7d35f2a5ee9
Jose Rene Palacios Ruiz <rene_xp_ruiz@hotmail.com>
2018-08-14 15:12:17 -0600

    Updated downloader.py file
    Refactor of download_files function and another key-elements of the data dictionary

--- Files-downloader/downloader.py
+++ Files-downloader/downloader.py
@@ -4,40 +4,52 @@ from requests_ntlm import HttpNtlmAuth
 
 user = 'User-01'
 password = 'passW00rd'
+today_date = dt.datetime.today().strftime('%m%d%y')
+free_day_format = dt.datetime.today().strftime('%Y-%m-%d')
 
 
-def getfilename(link, date_format):
-    today_date = dt.datetime.today().strftime(date_format)
-    data = link.split("/")
-    data = data[-1].split(".")
-    data = data[0] + " " + today_date + "." + data[1]
-    return data
+def getDateLines():
+    today = dt.datetime.today()
+    if today.weekday() == 0 | today.weekday() == 1:
+        date = today - dt.timedelta(days=4)
+        return date.strftime('%Y - %m-%d')
+    else:
+        date = today - dt.timedelta(days=2)
+        return date.strftime('%Y - %m-%d')
 
 
-def download_file(record):
+def download_files(dictionary):
     session = requests.Session()
     session.auth = HttpNtlmAuth(user, password)
-    r = session.get(record.get('link'))
-    record['newname'] = getfilename(record['link'], record['dateformat'])
-    with open(record['savein'] + record['newname'], 'wb') as f:
-        f.write(r.content)
+    for record in dictionary:
+        if dt.datetime.today().weekday() in record['recurrency']:
+            r = session.get(record.get('link'))
+            with open(record['savein'] + record['newname'], 'wb') as f:
+                f.write(r.content)
 
 
 files_dictionary = [
     {
         'key': 'excs',
+        'recurrency': [1, 3],
         'link': 'https://teams.organization.com/sharepoint/location a/excess report.xlsm',
         'savein': 'C:/path/to/save/file/',
-        'newname': '',
-        'dateformat': '%Y-%m-%d'
+        'newname': 'excess report' + today_date + '.xlsm'
     },
     {
         'key': 'excs2',
-        'link': 'https://teams.organization.com/sharepoint/location a/excess report2.xlsm',
+        'recurrency': [0, 1, 2, 3, 4],
+        'link': 'https://teams.organization.com/sharepoint/location a/excess report2 ' + getDateLines() + '.xlsm',
         'savein': 'C:/path/to/save/file2/',
-        'newname': '',
-        'dateformat': '%Y-%m-%d'
+        'newname': 'excess report2 ' + getDateLines() + '.xlsm'
     },
+    {
+        'key': 'excs3',
+        'recurrency': [4],
+        'link': 'https://teams.organization.com/sharepoint/location a/excess report3 ' + free_day_format + '.xlsb',
+        'savein': 'C:/path/to/save/file3/',
+        'newname': 'excess report3 ' + free_day_format + '.xlsb'
+    }
 ]
 
-download_file(files_dictionary[0])
+download_files(files_dictionary)
